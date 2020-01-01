9e4ca48539bd21938810f88244df9cf7cbfa2c9a
Jeffrey Brown <brownjs03@gmail.com>
2020-01-01 19:57:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,8 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "select temperament, count(temperament) where temperament = 'goofy';"
-end
+  "select temperament from bears"
 
 def selects_bear_that_killed_Tim
   "Write your SQL query here"
