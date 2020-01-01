38d12612b6b387ae1eb7078485aa5e38d8dd5ed3
Jeffrey Brown <brownjs03@gmail.com>
2020-01-01 19:17:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-"insert into bears (name) values ("Mr. Chocolate");"
+insert into bears (name) values ("Mr. Chocolate");
 insert into bears (name) values ("Rowdy")
 insert into bears (name) values ("Tabitha")
 insert into bears (name) values ("Sergeant Brown")
