2067bbe6cc9b1dffecc25595e644f597253eef30
Jeffrey Brown <brownjs03@gmail.com>
2020-01-01 19:30:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,9 +1,9 @@
 def selects_all_female_bears_return_name_and_age
-  "select name, age from bears where gender = 'F'"
+  "select name, age from bears where gender = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "select name from bears order by name asc"
+  "select name from bears order by name asc;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
